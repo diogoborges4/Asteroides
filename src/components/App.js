fc697c22ac82ef6025c424c7a59975eea3fff386
Diogo Borges <116685761+diogoborges4@users.noreply.github.com>
2023-09-02 14:45:32 -0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,7 @@ export default function App() {
   if (data.loading) {
     document.title = 'Counting potential earth HAZARDS…';
 
+  console.log(data)
     return (
       <p>
         Getting data from NASA right now to check whether something from space
